4e49981c0a67afa40294c31d66ec6ec13017e509
GithubESPI <a.vespuce@groupe-espi.fr>
2024-05-02 16:57:42 +0200

    Mise à jour des calculs de moyenne et d'ECTS

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -7,7 +7,19 @@ class Settings(BaseSettings):
     UPLOAD_DIR: str = os.path.join(BASE_DIR, "uploads")
     OUTPUT_DIR: str = os.path.join(BASE_DIR, "template", "S1")
     TEMPLATE_FILE: str = os.path.join(BASE_DIR, "template", "modeleM1S1.docx")
-    
+    ECTS_JSON_PATH: str = os.path.join(BASE_DIR, "json", "ects.json")  # Ajout du chemin JSON
+    RELEVANT_GROUPS: list = [
+        "N-M1 MAPI ALT 1", "P-M1 MAPI ALT 2", "L-M1 MAPI ALT 2", "MP-M1 MAPI ALT", 
+        "P-M1 MAPI ALT 5", "L-M1 MAPI ALT 1", "P-M1 MAPI ALT 1", "P-M1 MAPI ALT 3", 
+        "B-M1 MAPI ALT 1", "M-M1 MAPI ALT 1", "LI-M1 MAPI ALT", "N-M1 MAPI ALT 2", 
+        "M-M1 MAPI ALT 2", "P-M1 MAPI ALT 4", "B-M1 MAPI ALT 2", "MP-M1 MAPI ALT", 
+        "L-M1 MAPI ALT 3", "P-M1 MAGI ALT 1", "N-M1 MAGI ALT", "M-M1 MAGI ALT", 
+        "LI-M1 MAGI ALT", "B-M1 MAGI ALT", "MP-M1 MAGI ALT", "L-M1 MAGI ALT", 
+        "P-M1 MAGI ALT 2", "LI-M1 MAGI ALT", "P-M1 MAGI ALT 2", "M-M1 MIFIM ALT", 
+        "N-M1 MIFIM ALT", "P-M1 MIFIM ALT 1", "P-M1 MIFIM ALT 2", "P-M1 MIFIM ALT 3", 
+        "LI-M1 MIFIM ALT", "B-M1 MIFIM ALT", "MP-M1 MIFIM ALT", "L-M1 MIFIM ALT"
+    ]
+
     # Paramètres d'API externe
     YPAERO_BASE_URL: str
     YPAERO_API_TOKEN: str
@@ -18,3 +30,4 @@ class Settings(BaseSettings):
 
 # Instanciez les paramètres pour qu'ils soient importés et utilisés dans d'autres fichiers
 settings = Settings()
+
--- backend/app/services/word_service.py
+++ backend/app/services/word_service.py
@@ -1,45 +1,61 @@
 import logging
+import json
 import os
 import pandas as pd
-from docxtpl import DocxTemplate # Assurez-vous d'implémenter cette fonction
+from docxtpl import DocxTemplate
 from datetime import datetime  
+from app.core.config import settings
 
 # Configure logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+def read_ects_config():
+    with open(settings.ECTS_JSON_PATH, 'r') as file:
+        data = json.load(file)
+    return data['M1-S1'][0]
+
 def extract_grades_and_coefficients(grade_str):
     grades_coefficients = []
+    if grade_str.strip() == "":
+        return grades_coefficients  # Retourner une liste vide si la chaîne est vide
     parts = grade_str.split(" - ")
     for part in parts:
         if "Absent au devoir" in part:
             continue
         if "(" in part:
-            grade_part, coefficient_part = part.split("(")
-            coefficient = coefficient_part.replace(")", "").replace(",", ".")
-            grade = grade_part.replace(",", ".")
+            grade_part, coefficient_part = part[:-1].split("(")
+            grade = grade_part.replace(",", ".").strip()
+            coefficient = coefficient_part.replace(",", ".").strip()
         else:
-            grade = part.replace(",", ".")
+            grade = part.replace(",", ".").strip()
             coefficient = "1.0"
-        grades_coefficients.append((float(grade.strip()), float(coefficient.strip())))
+        try:
+            grades_coefficients.append((float(grade), float(coefficient)))
+            logger.debug(f"Extracted grade: {grade}, coefficient: {coefficient}")
+        except ValueError as e:
+            logger.error(f"Error converting grade/coefficient to float: {e}")
+            continue  # Passer à la partie suivante si une conversion échoue
     return grades_coefficients
 
-def calculate_weighted_average_from_string(grade_str):
-    grades_with_coefficients = extract_grades_and_coefficients(grade_str)
-    total_grade = 0.0
-    total_coefficient = 0.0
-    for grade, coefficient in grades_with_coefficients:
-        total_grade += grade * coefficient
-        total_coefficient += coefficient
-    return total_grade / total_coefficient if total_coefficient else 0
+def calculate_weighted_average(notes, ects):
+    if not notes or not ects:  # Vérifier que les listes ne sont pas vides
+        return 0.0
+    total_grade = sum(note * ects for note, ects in zip(notes, ects))
+    total_ects = sum(ects for ects in ects)
+    return total_grade / total_ects if total_ects != 0 else 0
 
 def generate_word_document(student_data, ects_values, titles_row, template_path, output_dir):
-    ects_values_as_int = [int(value) if pd.notna(value) else "NaN" for value in ects_values] 
+    ects_config = read_ects_config()
+    group_name = student_data["Nom Groupe"]
+    is_relevant_group = group_name in settings.RELEVANT_GROUPS
+    logger.debug(f"Processing student: {student_data['Nom']} in group: {group_name} - Relevant: {is_relevant_group}")
 
-    # Get current date
+    ects_values_as_int = [int(value) if pd.notna(value) else 0 for value in ects_values]
     current_date = datetime.now().strftime("%d/%m/%Y")
+
+    # Log the loaded ECTS values from JSON for verification
     
-    # Preparing the data for the placeholders
     placeholders = {
         "nomApprenant": student_data["Nom"],
         "etendugroupe": student_data["Étendu Groupe"],
@@ -49,6 +65,7 @@ def generate_word_document(student_data, ects_values, titles_row, template_path,
         "justifiee": student_data["ABS justifiées"],
         "injustifiee": student_data["ABS injustifiées"],
         "retard": student_data["Retards"],
+        "datedujour": current_date,
         "UE1_Title": titles_row[2],
         "matiere1": titles_row[5],
         "matiere2": titles_row[8],
@@ -69,14 +86,13 @@ def generate_word_document(student_data, ects_values, titles_row, template_path,
         "matiere13": titles_row[53],
         "matiere14": titles_row[56],
         "matiere15": titles_row[59],
-        "datedujour": current_date,   # Add the current date
         # ECTS
         # Ajout des placeholders pour les ECTS
-        "ECTSUE1": ects_values_as_int[0],
-        "ECTSUE2": ects_values_as_int[1],
-        "ECTSUE3": ects_values_as_int[2],
-        "ECTSUE4": ects_values_as_int[3],
-        "ECTSUE5": ects_values_as_int[4],
+        # "ECTSUE1": ects_values_as_int[0],
+        # "ECTSUE2": ects_values_as_int[1],
+        # "ECTSUE3": ects_values_as_int[2],
+        # "ECTSUE4": ects_values_as_int[3],
+        # "ECTSUE5": ects_values_as_int[4],
         "ECTS1": ects_values_as_int[5],
         "ECTS2": ects_values_as_int[6],
         "ECTS3": ects_values_as_int[7],
@@ -88,35 +104,55 @@ def generate_word_document(student_data, ects_values, titles_row, template_path,
         "ECTS9": ects_values_as_int[17],
         "ECTS10": ects_values_as_int[18],
         "ECTS11": ects_values_as_int[19],
-        # moyenne UE
-        # "moyUE1": f"{moy_ue1:.2f}",
     }
 
-
     all_grades_with_coefficients = []  # Pour calculer la moyenne générale à la fin
-    grade_column_indices = [5, 8, 11, 17, 23, 26, 32, 35, 38, 41, 44, 47, 53, 56, 59]  # Assurez-vous que ces indices correspondent
+    grade_column_indices = [5, 8, 11, 17, 23, 26, 32, 35, 38, 41, 44, 47, 53, 56, 59]
 
     for i, col_index in enumerate(grade_column_indices, start=1):
         grade_str = str(student_data.iat[col_index]).strip() if pd.notna(student_data.iat[col_index]) else ""
         if grade_str:
-            # Assurez-vous que cette fonction renvoie une liste de tuples (note, coefficient)
             grades_coefficients = extract_grades_and_coefficients(grade_str)
-            all_grades_with_coefficients.extend(grades_coefficients)  # Ajoutez les tuples à la liste générale
-
-            # Calculer et afficher la moyenne individuelle
+            all_grades_with_coefficients.extend(grades_coefficients)
             individual_average = sum(grade * coefficient for grade, coefficient in grades_coefficients) / sum(coefficient for grade, coefficient in grades_coefficients) if grades_coefficients else 0
             placeholders[f"note{i}"] = f"{individual_average:.2f}"
 
-    # Calcul de la moyenne générale pour toutes les notes et coefficients collectés
+    all_grades_with_coefficients = []
+    # Logique pour parcourir chaque matière
+    for i in range(1, 16):  # Supposons qu'il y ait 15 matières
+        subject_key = f"matiere{i}"
+        if subject_key in student_data and student_data[subject_key]:
+            grade_str = student_data[subject_key]
+            grades_coefficients = extract_grades_and_coefficients(grade_str)
+            if grades_coefficients:
+                individual_average = calculate_weighted_average([g[0] for g in grades_coefficients], [g[1] for g in grades_coefficients])
+                ects_key = f'ECTS{i}'
+                placeholders[subject_key] = f"{individual_average:.2f}"
+                placeholders[ects_key] = float(ects_config.get(ects_key, 0)) if individual_average >= 8 and is_relevant_group else 0
+                logger.debug(f"Processed {subject_key}: Avg = {individual_average}, ECTS = {placeholders[ects_key]}")
+            else:
+                placeholders[subject_key] = "N/A"  # Notation N/A pour les matières sans notes valides
+                placeholders[f'ECTS{i}'] = 0
+                logger.debug(f"No valid grades found for {subject_key}. Set ECTS to 0.")
+        else:
+            logger.debug(f"{subject_key} is missing or empty in student_data.")
+
+
+
+
+    # Calculate general average
     if all_grades_with_coefficients:
         general_average = sum(grade * coefficient for grade, coefficient in all_grades_with_coefficients) / sum(coefficient for grade, coefficient in all_grades_with_coefficients)
-        placeholders["moyenne"] = f"{general_average:.2f}"
-    else:
-        placeholders["moyenne"] = ""
+        placeholders['moyenne'] = general_average
 
-    # Génération du document avec les placeholders remplis
+    # Process notes and ECTS for UEs
+    for ue, (start, end) in enumerate([(1, 3), (4, 4), (5, 6), (7, 12), (13, 15)], start=1):
+        notes = [float(student_data.get(f'note{i}', 0)) for i in range(start, end+1)]
+        ects = [float(ects_config.get(f'ECTS{i}', 0)) for i in range(start, end+1) if is_relevant_group]
+        moy_ue = calculate_weighted_average(notes, ects)
+        placeholders[f'moyUE{ue}'] = f"{moy_ue:.2f}"
+
     doc = DocxTemplate(template_path)
-    # Génération du document avec les placeholders remplis
     doc.render(placeholders)
     
     output_filename = f"{student_data['Nom']}_bulletin.docx"
