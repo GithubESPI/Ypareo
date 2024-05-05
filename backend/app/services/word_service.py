44d776672aed1c96349fb6989b96602fc6d014b0
GithubESPI <a.vespuce@groupe-espi.fr>
2024-05-05 19:25:08 +0200

    Mise à jour du word_service.py

--- backend/app/services/word_service.py
+++ backend/app/services/word_service.py
@@ -45,6 +45,7 @@ def generate_word_document(student_data, titles_row, template_path, output_dir):
     current_date = datetime.now().strftime("%d/%m/%Y")
     group_name = student_data["Nom Groupe"]
     is_relevant_group = group_name in settings.RELEVANT_GROUPS
+    logger.debug("Processing document for group: %s", group_name)
 
     placeholders = {
         "nomApprenant": student_data["Nom"],
@@ -78,11 +79,14 @@ def generate_word_document(student_data, titles_row, template_path, output_dir):
         "matiere15": titles_row[59]
     }
 
-
-    # Indices des colonnes de notes dans Excel
     grade_column_indices = [5, 8, 11, 17, 23, 26, 32, 35, 38, 41, 44, 47, 53, 56, 59]
-
-    all_grades_with_coefficients = []
+    ects_sum_indices = {
+        'ECTSUE1': [1, 2, 3],
+        'ECTSUE2': [4],
+        'ECTSUE3': [5, 6],
+        'ECTSUE4': [7, 11],
+        'ECTSUE5': [13, 14, 15]
+    }
 
     for i, col_index in enumerate(grade_column_indices, start=1):
         grade_str = str(student_data.iat[col_index]).strip() if pd.notna(student_data.iat[col_index]) else ""
@@ -91,20 +95,22 @@ def generate_word_document(student_data, titles_row, template_path, output_dir):
             individual_average = calculate_weighted_average([g[0] for g in grades_coefficients], [g[1] for g in grades_coefficients])
             placeholders[f"note{i}"] = f"{individual_average:.2f}" if individual_average else ""
             if individual_average >= 8 and is_relevant_group:
-                placeholders[f"ECTS{i}"] = ects_config.get(f"ECTS{i}", 0)
-                if placeholders[f"ECTS{i}"] == 1:
-                    placeholders[f"ECTS{i}"] = ""
-                if not placeholders[f"note{i}"]:
-                    placeholders[f"ECTS{i}"] = ""
+                ects_value = int(ects_config.get(f"ECTS{i}", 0))  # Assurez-vous que c'est un entier
+                placeholders[f"ECTS{i}"] = ects_value
             else:
                 placeholders[f"ECTS{i}"] = 0
         else:
             placeholders[f"note{i}"] = ""
-            placeholders[f"ECTS{i}"] = ""
+            placeholders[f"ECTS{i}"] = 0
 
-    if all_grades_with_coefficients:
-        general_average = sum(grade * coefficient for grade, coefficient in all_grades_with_coefficients) / sum(coefficient for grade, coefficient in all_grades_with_coefficients)
-        placeholders['moyenne'] = f"{general_average:.2f}"
+    # Calcul des ECTS par UE
+    for ue, indices in ects_sum_indices.items():
+        sum_ects = sum(int(placeholders.get(f"ECTS{i}", 0)) for i in indices)  # Conversion en entier
+        placeholders[ue] = sum_ects
+    
+    # Calcul de la moyenne totale des ECTS
+    total_ects = sum(placeholders[ue] for ue in ects_sum_indices.keys())
+    placeholders['moyenneECTS'] = total_ects
 
     doc = DocxTemplate(template_path)
     doc.render(placeholders)
